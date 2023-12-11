10735c141d77c6c2f68ff60b7136d3ad0d9fe8d4
钟宇强 <zhongyuqiang@mail.com>
2023-12-12 03:21:09 +0800

    fix: finish

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,5 @@ import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [astroI18next()],
-  redirects: {
-    "/": "/ground/"
-  }
+  integrations: [astroI18next()]
 });
